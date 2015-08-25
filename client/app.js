42d1ed901a483174e85dcaae724c79b9739c03db
Paul Laskowski <pelaskowski@gmail.com>
2015-08-25 16:36:31 -0700

    UI and ux enhancements added code for sticky sidebar layed out with flexbox lots of styling and pics added

---
+++ client/app.js
@@ -0,0 +1,27 @@
+var scroller = function() {
+  var sbPos = $('body').scrollTop();
+  var topThresh = $('.sticky-col').offset().top;
+  var botThresh = topThresh + $('.sticky-col').height() - $('.sticky-content').height() - (parseInt($('.sticky-content').css('padding')) * 2);
+
+  $('div.scroll-output').text('scrollTop:' + sbPos);
+
+  if(sbPos > topThresh) {
+    if(sbPos > botThresh) {
+      // stick-to-bot only
+      $('.sticky-content').removeClass('stick-to-top');
+      $('.sticky-content').addClass('stick-to-bot');
+    } else {
+      // stick-to-top only
+      $('.sticky-content').removeClass('stick-to-bot');
+      $('.sticky-content').addClass('stick-to-top');
+    }
+  } else {
+    //no stick-to class
+    $('.sticky-content').removeClass('stick-to-top');
+    $('.sticky-content').removeClass('stick-to-bot');
+  }
+};
+Meteor.startup(function(){
+  scroller();
+  $(window).scroll(scroller);
+});
