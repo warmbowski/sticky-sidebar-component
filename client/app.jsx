653a849d0188f5290e7da9506d719a5aad4a390a
Paul Laskowski <pelaskowski@gmail.com>
2015-08-27 15:03:09 -0700

    moved scroll function to Aside component handleScroll removed all jQuery;

--- client/app.jsx
+++ client/app.jsx
@@ -1,25 +1,5 @@
 var scroller = function() {
-  var stickyCol = document.getElementById('rightbar');
-  var stickyColBox = stickyCol.getBoundingClientRect();
-  var stickyContentBox = stickyCol.childNodes[0].getBoundingClientRect();
-
-  if(stickyContentBox.bottom >= stickyColBox.bottom ) {
-    if(stickyColBox.top <= 0 && stickyContentBox.top > 0) {
-      $('.sticky-content').addClass('stick-to-top');
-      $('.sticky-content').removeClass('stick-to-bot');
-    } else {
-      $('.sticky-content').addClass('stick-to-bot');
-      $('.sticky-content').removeClass('stick-to-top');
-    }
-  } else {
-    if(stickyContentBox.top >= 0 && stickyColBox.top >= 0) {
-      $('.sticky-content').removeClass('stick-to-top');
-      $('.sticky-content').removeClass('stick-to-bot');
-    } else {
-      $('.sticky-content').addClass('stick-to-top');
-      $('.sticky-content').removeClass('stick-to-bot');
-    }
-  }
+  
 };
 
 Meteor.startup(function(){
--- client/components/Aside.jsx
+++ client/components/Aside.jsx
@@ -1,4 +1,37 @@
 Aside = React.createClass({
+  componentDidMount: function() {
+      window.addEventListener('scroll', this.handleScroll);
+  },
+
+  componentWillUnmount: function() {
+      window.removeEventListener('scroll', this.handleScroll);
+  },
+
+  handleScroll: function(evt) {
+    console.log(this.getDOMNode());
+    var stickyCol = this.getDOMNode();
+    var stickyColBox = stickyCol.getBoundingClientRect();
+    var stickyContent = stickyCol.childNodes[0];
+    var stickyContentBox = stickyContent.getBoundingClientRect();
+
+    if(stickyContentBox.bottom >= stickyColBox.bottom ) {
+      if(stickyColBox.top <= 0 && stickyContentBox.top > 0) {
+        stickyContent.classList.add('stick-to-top');
+        stickyContent.classList.remove('stick-to-bot');
+      } else {
+        stickyContent.classList.add('stick-to-bot');
+        stickyContent.classList.remove('stick-to-top');
+      }
+    } else {
+      if(stickyContentBox.top >= 0 && stickyColBox.top >= 0) {
+        stickyContent.classList.remove('stick-to-top');
+        stickyContent.classList.remove('stick-to-bot');
+      } else {
+        stickyContent.classList.add('stick-to-top');
+        stickyContent.classList.remove('stick-to-bot');
+      }
+    }
+  },
 
   render: function() {
     return (
