fe034b95e683eed50ab02553125a49ae4d3f0ba6
Paul Laskowski <pelaskowski@gmail.com>
2015-08-27 14:32:51 -0700

    refactored scroller function to use .getBoundingClientRect()

--- client/app.jsx
+++ client/app.jsx
@@ -1,25 +1,27 @@
 var scroller = function() {
-  var sbPos = $(window).scrollTop();
-  var topThresh = $('.sticky-col').offset().top;
-  var botThresh = topThresh + $('.sticky-col').height() - $('.sticky-content').height() - (parseInt($('.sticky-content').css('paddingTop')) * 2);
-  $('div.scroll-output').text('scrollTop:' + sbPos);
+  var stickyCol = document.getElementById('rightbar');
+  var stickyColBox = stickyCol.getBoundingClientRect();
+  var stickyContentBox = stickyCol.childNodes[0].getBoundingClientRect();
 
-  if(sbPos > topThresh) {
-    if(sbPos > botThresh) {
-      // stick-to-bot only
-      $('.sticky-content').removeClass('stick-to-top');
-      $('.sticky-content').addClass('stick-to-bot');
-    } else {
-      // stick-to-top only
-      $('.sticky-content').removeClass('stick-to-bot');
+  if(stickyContentBox.bottom >= stickyColBox.bottom ) {
+    if(stickyColBox.top <= 0 && stickyContentBox.top > 0) {
       $('.sticky-content').addClass('stick-to-top');
+      $('.sticky-content').removeClass('stick-to-bot');
+    } else {
+      $('.sticky-content').addClass('stick-to-bot');
+      $('.sticky-content').removeClass('stick-to-top');
     }
   } else {
-    //no stick-to class
-    $('.sticky-content').removeClass('stick-to-top');
-    $('.sticky-content').removeClass('stick-to-bot');
+    if(stickyContentBox.top >= 0 && stickyColBox.top >= 0) {
+      $('.sticky-content').removeClass('stick-to-top');
+      $('.sticky-content').removeClass('stick-to-bot');
+    } else {
+      $('.sticky-content').addClass('stick-to-top');
+      $('.sticky-content').removeClass('stick-to-bot');
+    }
   }
 };
+
 Meteor.startup(function(){
   React.render(<ContentContainer />, document.getElementById('yield-content-container'));
   scroller();
